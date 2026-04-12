ae6f020232377075deda89e0830b1c6269c3e0d3
Huan Jiang <huanji@amazon.com>
2026-04-12 20:17:55 +0000

    chore: Commit pending changes before stash pop

--- crates/fuse-server/src/graphql.rs
+++ crates/fuse-server/src/graphql.rs
@@ -510,6 +510,8 @@ mod tests {
             query_recorder: Arc::new(crate::query_replay::QueryRecorder::new(1000)),
             compilation_cache: Arc::new(crate::query_compilation::CompilationCache::new(300, 5000)), cdc_tracker: Arc::new(crate::cdc::CdcTracker::new(1000)),
             adaptive_cache: Arc::new(crate::adaptive_cache::AdaptiveCache::new(60, 3, 10000)), column_rbac: None,
+            column_rbac: None,
+            key_rotation: Arc::new(crate::auth::KeyRotationManager::new(vec![])),
         })
     }
 
@@ -671,6 +673,8 @@ mod subscription_tests {
             compilation_cache: Arc::new(crate::query_compilation::CompilationCache::new(300, 5000)),
             cdc_tracker: Arc::new(crate::cdc::CdcTracker::new(1000)),
             adaptive_cache: Arc::new(crate::adaptive_cache::AdaptiveCache::new(60, 3, 10000)),
+            column_rbac: None,
+            key_rotation: Arc::new(crate::auth::KeyRotationManager::new(vec![])),
         })
     }
 
